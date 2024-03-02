384815579d1841da08b878ebb99f4334363b6d00
gio <145365018+giorgiaespa@users.noreply.github.com>
2024-03-02 20:56:00 +0100

    adv ex 5

---
+++ deploy1/src/main/java/com/advEx5/deploy1/Controller.java
@@ -0,0 +1,17 @@
+package com.advEx5.deploy1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private Environment environment;
+    @GetMapping("/get")
+    private String printVar(){
+        return environment.getProperty("myProperty.devName");
+    }
+}
