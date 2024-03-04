ca535e96ff60788fb9c7e3d8bcc969a173213f62
gio <145365018+giorgiaespa@users.noreply.github.com>
2024-03-04 03:36:59 +0100

    adv ex 7

---
+++ deploy3/src/main/java/com/advex7/deploy3/Deploy3Application.java
@@ -0,0 +1,13 @@
+package com.advex7.deploy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy3Application.class, args);
+	}
+
+}
---
+++ deploy3/src/test/java/com/advex7/deploy3/Deploy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advex7.deploy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
