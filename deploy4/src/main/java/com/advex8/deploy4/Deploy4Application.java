5c3a806703ca4cfec40cd6e929e5fe8baff1640a
gio <145365018+giorgiaespa@users.noreply.github.com>
2024-03-04 03:37:35 +0100

    adv ex 8

---
+++ deploy4/src/main/java/com/advex8/deploy4/Deploy4Application.java
@@ -0,0 +1,13 @@
+package com.advex8.deploy4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deploy4Application.class, args);
+	}
+
+}
---
+++ deploy4/src/test/java/com/advex8/deploy4/Deploy4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advex8.deploy4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
