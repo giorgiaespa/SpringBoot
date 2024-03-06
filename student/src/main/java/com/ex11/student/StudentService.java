b7e7e100bf4c06086089af57bc83e83fda840e8f
gio <145365018+giorgiaespa@users.noreply.github.com>
2024-03-06 20:06:09 +0100

    ex 11

--- student/src/main/java/com/ex11/student/StudentService.java
+++ student/src/main/java/com/ex11/student/StudentService.java
@@ -9,7 +9,28 @@ import java.util.Optional;
 @Service
 public class StudentService {
     @Autowired
-    private StudentRepository studentRepository;
+    StudentRepository studentRepository;
+
+    public Student newStudent(Student student) {
+        return studentRepository.saveAndFlush(student);
+    }
+
+    public List<Student> findAll(){
+        return studentRepository.findAll();
+    }
+
+    public Student findOne (Long id){
+        Optional <Student> student = studentRepository.findById(id);
+        return student.orElse(null);
+    }
+
+//    public static Student newId(Long id, Student student) {
+//        if (student != null) {
+//            student.setId(id);
+//            return studentRepository.saveAndFlush(student);
+//        }
+//        return null;
+//    }
     public Student updateStatus (Long id,boolean isWorking) {
         Optional<Student>workingStudent = studentRepository.findById(id);
         if (workingStudent.isPresent()) {
@@ -17,5 +38,9 @@ public class StudentService {
             return studentRepository.save(workingStudent.get());
         }
         return null;
+    }
+
+    public void deletedById (Long id) {
+        studentRepository.deleteById(id);
     }
 }
